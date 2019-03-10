3c88e806bc225d048ddbf13773a423a1c5148a84
Natalie Halawani <hala1224@gmail.com>
2019-03-10 02:54:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,40 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do
     erb :new
   end
-  
+
+  post '/articles' do
+    @post = Article.create(params)
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @articles = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @articles = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.name = params[:name]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+
+  post '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
   
 end
